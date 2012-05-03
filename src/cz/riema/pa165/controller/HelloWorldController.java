8d8b8c85ff25b35b443fa6a4c20fc4e0c33c5c80
Marie Hrdinova <riema.sai@gmail.com>
2012-05-03 19:05:08 +0200

    Přidán HelloWorld ve springu. Balík na kontrolery a složka na jsp.

---
+++ src/cz/riema/pa165/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package cz.riema.pa165.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public final class HelloWorldController {
+ 
+	@RequestMapping(value = "/hello",  method = RequestMethod.GET)
+	public String HelloWorld() {
+		return "MyView";
+	}
+}
